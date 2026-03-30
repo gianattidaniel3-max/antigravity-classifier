353864d32bdac86f336a736b8325abd918e8ca1e
gianattidaniel3-max <gianatti.daniel3@gmail.com>
2026-03-30 19:06:49 +0200

    fix: ultra-robust directory management for Admin setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,8 @@
 @echo off
-cd /d "%~dp0"
+pushd "%~dp0"
 TITLE ECO - Setup
 echo --- Avvio Setup ECO ---
+echo Caricamento script PowerShell...
 powershell -NoProfile -ExecutionPolicy Bypass -File "setup_windows.ps1"
+popd
 pause
